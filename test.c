0b70e6f697de16b7093b30180a03f51d496ba1a4
capreyon <sudh.cse@gmail.com>
2018-06-14 10:31:54 +0530

    verify bit test emulation

--- test.c
+++ test.c
@@ -391,6 +391,39 @@ main()
 	assert(err == 0);
 	assert(mc.val == 0xa0aa);
 
+	/*
+	 * (rax = lapic address, 0xff000000)
+         * BT   $ff,0xf0(%rax)             || BT r/m16/32/64, imm8
+         * 0xBA 0xf0 0x00 0x00 0x00 0xff
+         */
+	memset(&vie, 0, sizeof(struct vie));
+	vie.base_register = VM_REG_LAST;
+	vie.index_register = VM_REG_LAST;
+
+	vm_regs[VM_REG_GUEST_RAX] = 0xff000000;
+	vm_regs[VM_REG_GUEST_RFLAGS]=0xff000000;
+	vie.inst[0] = 0xBA;
+	vie.inst[1] = 0xf0;
+	vie.inst[2] = 0x00;
+	vie.inst[3] = 0x00;
+	vie.inst[4] = 0x00;
+	vie.inst[5] = 0xff;
+	vie.num_valid = 6;
+
+	gla = 0;
+	err = vmm_decode_instruction(NULL, 0, gla, &vie);
+	assert(err == 0);
+
+	mc.addr = 0xff0000f0;
+	mc.val  = 0x0000a1aa;
+	gpa = 0xff0000f0;
+	err = vmm_emulate_instruction(NULL, 0, gpa, &vie,
+				      test_mread, test_mwrite,
+				      &mc);
+	assert(err == 0);
+	assert(mc.val == 0xa0aa);
+
+
 
 
 }
