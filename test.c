f131fd295a5f19427782a264b9ff510a18bbbc6d
capreyon <sudh.cse@gmail.com>
2018-06-29 12:55:48 +0530

    Test for movzx r32, r/m8 0xB6 (Mov with zero-extend)

--- test.c
+++ test.c
@@ -657,6 +657,43 @@ main()
 	assert(err == 0);
 	assert(mc.val == 0xdeadbeef);
 
+	/* 
+	 * ICLASS: MOVZX            CATEGORY: DATAXFER            EXTENSION: BASE          IFORM: MOVZX_GPRv_MEMb           ISA_SET: I386
+     * SHORT: movzx ax, byte ptr [ecx+0x58ecdc05]            MoVZX r32, r/m8 (Mov with Zero-extend)
+     * 0f b6 81 05 dc ec 58
+	 * rip -> ffffffff813d2751
+	 * val -> ffffffff8196c0f0
+	 * pa -> 0xfee000f0
+	 */
+	memset(&vie, 0, sizeof(struct vie));
+	vie.base_register = VM_REG_LAST;
+	vie.index_register = VM_REG_LAST;
+
+	/* RIP-relative is from next instruction */
+	vm_regs[VM_REG_GUEST_RIP] = 0xffffffff8046539d + 7;	
+	vie.inst[0] = 0x0f;
+	vie.inst[1] = 0xb6;
+	vie.inst[2] = 0x81;
+	vie.inst[3] = 0x05;
+	vie.inst[4] = 0xdc;
+	vie.inst[5] = 0xec;
+	vie.inst[6] = 0x58;
+	vie.num_valid = 7;
+
+    gla = 0;
+    err = vmm_decode_instruction(NULL, 0, gla, &vie);
+    assert(err == 0);
+
+    mc.addr = 0xfee000f0;
+	mc.val  = 0;
+	gpa = 0xfee000f0;
+	err = vmm_emulate_instruction(NULL, 0, gpa, &vie,
+				      test_mread, test_mwrite,
+				      &mc);
+	assert(err == 0);
+	assert(mc.val == 0xdeadbeef);
+
+
     /*
 	 * (rcx = lapic address, 0xff000000)  
 	 * or    0xf0(%rcx),%eax              || or r/m, reg
